038ac25c1ac7077cdfe646fc6b93b16ab8513585
MasterKia <mysignup27@gmail.com>
2016-04-14 18:46:29 +0430

    Update ctrl.lua

--- plugins/ctrl.lua
+++ plugins/ctrl.lua
@@ -26,13 +26,13 @@ local function enable_channel(receiver, to_id)
 	end
 
 	if _config.disabled_channels[receiver] == nil then
-		return 'Xy Enabled !'
+		return '✅Xy Enabled !'
 	end
 	
 	_config.disabled_channels[receiver] = false
 
 	save_config()
-	return 'Xy Enabled !'
+	return '✅Xy Enabled !'
 end
 
 local function disable_channel(receiver, to_id)
@@ -43,7 +43,7 @@ local function disable_channel(receiver, to_id)
 	_config.disabled_channels[receiver] = true
 
 	save_config()
-	return 'Xy Disabled !'
+	return '❌Xy Disabled !'
 end
 
 local function pre_process(msg)
